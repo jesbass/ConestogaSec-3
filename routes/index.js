2141ca1a6f989280bf69c59057485b1cda6b1eeb
jesbass <jesbass@gmail.com>
2021-08-03 19:45:18 -0400

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jesus Cuevas Portfolio' });
 });
-
+/*
 router.get('/about', (req,res,next) => {
   res.render('about', { title: 'About', 
   pageText: 'Here is some information about this project' })
 })
-
+*/
 module.exports = router;
